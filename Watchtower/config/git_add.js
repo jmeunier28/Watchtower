e10d2e80cc6f09ca7e3dc2b0643a24a40812e24f
Pranay Singh <pranayrs@bu.edu>
2016-04-30 13:56:20 -0400

    Update git_add.js

--- Watchtower/config/git_add.js
+++ Watchtower/config/git_add.js
@@ -1,3 +1,5 @@
+
+
 var Github = require('github-api')
 var uuid = require('uuid4');
 
@@ -5,12 +7,12 @@ var uuid = require('uuid4');
 module.exports = function(name){
 
 var github = new Github({
-	username: "Github User",
-	password: "Github pass",
-	auth: "Github Auth"
+	username: "Github User",  // Edit this with your Github username
+	password: "Github pass",  // Edit this with you Github pass.
+	auth: "Github Auth"  // Edit this with you API Auth token from https://github.com/settings/tokens/new
 })
 
-var repo = github.getRepo("Github Username", "Your Github repo");
+var repo = github.getRepo("Github Username", "Your Github repo"); // Edit this with your Github username and desire repo name
 var run=function(i){
 	if(i <= name.no){
 		path = name.path + '/' + name.doc + '-' + i;
