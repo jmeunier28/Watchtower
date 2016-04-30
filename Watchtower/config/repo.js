c6c7a837c51e6361cb43ba4120323282f216c585
Pranay Singh <pranayrs@bu.edu>
2016-04-30 13:57:29 -0400

    Update repo.js

--- Watchtower/config/repo.js
+++ Watchtower/config/repo.js
@@ -5,12 +5,12 @@ var uuid = require('uuid4');
 module.exports = function(name){
 
 var github = new Github({
-	username: "Github User",
-	password: "Github pass",
-	auth: "Github Auth"
+	username: "Github User", // Edit this with you github username
+	password: "Github pass", // Edit this with your Github pass
+	auth: "Github Auth" // Edit this with Github API token from https://github.com/settings/tokens/new
 })
 
-var repo = github.getRepo("Your Github username", "Your Github repo");
+var repo = github.getRepo("Your Github username", "Your Github repo"); // Edit this with you Username and desired repo
 var id = uuid(); 
 repo.write('master', name , id , id , function(err) {
 	console.log(err);
